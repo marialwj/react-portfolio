a724b2133d7281ef01d8c584ff747cb2d4ccce05
marialwj <mariaa.georgy@gmail.com>
2024-04-06 20:11:41 -0700

    fix vite build issue

--- react-portfolio/vite.config.js
+++ react-portfolio/vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -11,6 +10,9 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true 
+  },
+  build: {
+    outDir: '../dist'
+}
+});
